d3bb3623658643923e7faedad087a47d264e9bdc
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-03 02:46:26 +0530

    first commit

---
+++ spring-boot-producer/src/test/java/com/example/spring_boot_producer/SpringBootProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
