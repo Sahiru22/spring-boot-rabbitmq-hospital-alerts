2042d6c74cdc9abbb57ffede02228ba593d7adad
Sahiru22 <sahirubalasuriya222@gmail.com>
2025-08-03 16:55:04 +0530

    add fanout producer

--- spring-boot-producer/src/main/java/com/example/spring_boot_producer/controller/HospitalAlertController.java
+++ spring-boot-producer/src/main/java/com/example/spring_boot_producer/controller/HospitalAlertController.java
@@ -1,4 +1,19 @@
 package com.example.spring_boot_producer.controller;
 
+import com.example.spring_boot_producer.producer.HospitalAlertProducer;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/alert")
+@AllArgsConstructor
 public class HospitalAlertController {
+
+  private final HospitalAlertProducer producer;
+
+  @GetMapping("/send")
+  public String sendAlert(@RequestParam String message) {
+    producer.sendHospitalAlert(message);
+    return "Alert Sent: " + message;
+  }
 }
--- spring-boot-producer/src/main/java/com/example/spring_boot_producer/producer/HospitalAlertProducer.java
+++ spring-boot-producer/src/main/java/com/example/spring_boot_producer/producer/HospitalAlertProducer.java
@@ -1,4 +1,21 @@
 package com.example.spring_boot_producer.producer;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HospitalAlertProducer {
+
+  private static final String FANOUT_EXCHANGE = "hospital-alert-exchange";
+
+  private final RabbitTemplate rabbitTemplate;
+
+  public HospitalAlertProducer(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  public void sendHospitalAlert(String message) {
+    rabbitTemplate.convertAndSend(FANOUT_EXCHANGE, "", message);
+    System.out.println("Broadcast hospital alert: " + message);
+  }
 }
